11c30e93e0a0c763bae150c08c76d579a0e5a640
arkarmoe <arkarmoe17@gmail.com>
2022-03-08 16:56:17 +0630

    create language remove component

---
+++ src/app/components/language/language-remove/language-remove.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-language-remove',
+  templateUrl: './language-remove.component.html',
+  styleUrls: ['./language-remove.component.css']
+})
+export class LanguageRemoveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
